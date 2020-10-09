e026026ae0f6240ed2cbfdbc82a6d35f5bd826a9
Nikhil Supekar <nikhilvsupekar@gmail.com>
2020-10-09 13:46:04 -0400

    loader optimization

--- dev/conv3d_encdec/loader.py
+++ dev/conv3d_encdec/loader.py
@@ -1,3 +1,5 @@
+import os
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,22 +10,16 @@ import h5py
 
 
 class SingleChannelDataset(torch.utils.data.Dataset):
-    def __init__(self, file_path):
-        self.file_path = file_path
-        file = h5py.File(self.file_path, 'r')
-
-        self.input_np = np.array(file['Train']['input'])
-        self.target_np = np.array(file['Train']['target'])
-
-        assert self.input_np.shape[0] == self.target_np.shape[0]
-
-    def __len__(self):
-        return self.input_np.shape[0]
+    def __init__(self, data_dir):
+        self.data_dir = data_dir
 
     def __getitem__(self, index):
-        input_1 = self.input_np[index, 0, :, :, :]
-        input_2 = self.input_np[index, 1, :, :, :]
-        target = self.target_np[index, 0, :, :, :]
+        frame_path = os.path.join(self.data_dir, f'frame_{index}', 'frame_data.hdf5')
+        frame_file = h5py.File(frame_path)
+        
+        input1 = frame_file['input1']
+        input2 = frame_file['input2']
+        target = frame_file['target']
 
         return torch.tensor(input_1).unsqueeze(0).float(), torch.tensor(input_2).unsqueeze(0).float(), torch.tensor(target).unsqueeze(0).float()
 
